1f8f7c058138da887d1a7e104c770744381edc16
Jonny Tullis <jon.tullis558@gmail.com>
2020-09-29 13:22:37 -0600

    Added recursive calls and GUI displays. Algorithms working as expected.

--- convex_hull.py
+++ convex_hull.py
@@ -22,6 +22,34 @@ PAUSE = 0.25
 #
 # This is the class you have to complete.
 #
+def get_inner_indexes(left, right):
+    largest_list_size = 0
+    if len(left) > len(right):
+        largest_list_size = len(left)
+    else:
+        largest_list_size = len(right)
+
+    # Get the far right point of the left list and the far left point of the right list O(n)
+    right_index = left_index = -1
+    for i in range(largest_list_size):
+        if i < len(right):
+            if right_index < 0:
+                right_index = i
+            elif right[i].x() < right[right_index].x():
+                right_index = i
+        if i < len(left):
+            if left_index < 0:
+                left_index = i
+            elif left[i].x() > left[left_index].x():
+                left_index = i
+    return left_index, right_index
+
+
+def get_slope(point1, point2):
+    assert type(point1) == QPointF and type(point2) == QPointF
+    return (point1.y() - point2.y()) / (point1.x() - point2.x())
+
+
 class ConvexHullSolver(QObject):
 
     # Class constructor
@@ -42,9 +70,9 @@ class ConvexHullSolver(QObject):
 
     def blinkTangent(self, line, color):
         self.showTangent(line, color)
-        self.showHull([], BLUE)
+        if self.pause:
+            self.showHull([], BLUE)
         self.eraseTangent(line)
-        self.showHull([], BLUE)
 
     def showHull(self, polygon, color):
         self.view.addLines(polygon, color)
@@ -66,134 +94,109 @@ class ConvexHullSolver(QObject):
         assert (type(points) == list and type(points[0]) == QPointF)
 
         t1 = time.time()
-        # Sort points by x value
-        points.sort(key=lambda point: point.x())
+        points.sort(key=lambda point: point.x())  # Sort points by x value
+        hull = self.divide_and_conquer_solver(points)
         t2 = time.time()
 
-        t3 = time.time()
-        # this is a dummy polygon of the first 3 unsorted points
-        polygon = [QLineF(points[i], points[(i + 1) % 3]) for i in range(3)]
-        self.divide_and_conquer_solver(points)
-        t4 = time.time()
-
-        # when passing lines to the display, pass a list of QLineF objects.  Each QLineF
-        # object can be created with two QPointF objects corresponding to the endpoints
-        # self.showHull(polygon, RED)
-        self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t4 - t3))
+        polygon = [QLineF(hull[i], hull[(i + 1) % len(hull)]) for i in range(len(hull))]
+        self.showHull(polygon, GREEN)
+        self.showText('Time Elapsed (Convex Hull): {:3.3f} sec'.format(t2 - t1))
 
     def divide_and_conquer_solver(self, points):
-        # if len(points) == 1:
-        #     return points[0]
-        # left_half = points[:len(points) // 2]
-        # right_half = points[len(points) // 2:]
-        # left = self.divide_and_conquer_solver(left_half)
-        # right = self.divide_and_conquer_solver(right_half)
-        # return self.combine_hulls(left, right)
-
-        left_half = self.sort_clockwise(points[1:4])
-        right_half = self.sort_clockwise(points[4:7])
-
-        polygon1 = [QLineF(left_half[i], left_half[(i + 1) % len(left_half)]) for i in range(len(left_half))]
-        polygon2 = [QLineF(right_half[i], right_half[(i + 1) % len(right_half)]) for i in range(len(right_half))]
-
-        self.showHull(polygon1, BLUE)
-        self.showHull(polygon2, BLUE)
-
-        self.combine_hulls(left_half, right_half)
+        if len(points) == 1:
+            return points
+        left_half = points[:len(points) // 2]
+        right_half = points[len(points) // 2:]
+        left = self.divide_and_conquer_solver(left_half)
+        right = self.divide_and_conquer_solver(right_half)
+        return self.combine_hulls(left, right)
 
     # O(n) time complexity
     def combine_hulls(self, left, right):
-        if len(left) == 0:
-            return right or []
-        if len(right) == 0:
-            return left or []
+        # GUI Display
+        polygon1, polygon2, line1, line2 = None, None, None, None
+        if self.pause:
+            polygon1 = [QLineF(left[i], left[(i + 1) % len(left)]) for i in range(len(left))]
+            polygon2 = [QLineF(right[i], right[(i + 1) % len(right)]) for i in range(len(right))]
+            self.showHull(polygon1, BLUE)
+            self.showHull(polygon2, BLUE)
 
-        upper_tangent = self.get_tangent_indexes(left, right, True)
-        lower_tangent = self.get_tangent_indexes(left, right, False)
+        upper_left, upper_right = self.get_tangent_indexes(left, right, True)
+        # Show upper tangent on GUI
+        if self.pause:
+            line1 = QLineF(left[upper_left], right[upper_right])
+            self.showTangent([line1], GREEN)
+
+        lower_left, lower_right = self.get_tangent_indexes(left, right, False)
+        # Show lower tangent on GUI
+        if self.pause:
+            line2 = QLineF(left[lower_left], right[lower_right])
+            self.showTangent([line2], GREEN)
+
+        new_hull = []
+
+        # Add valid hull indexes from right hull (everything clockwise between upper_right and lower_right)
+        i = upper_right
+        while i != lower_right:
+            new_hull.append(right[i])
+            i = (i + 1) % len(right)  # next index clockwise
+        new_hull.append(right[i])  # add the lower_right index
+
+        # Add valid hull indexes from left hull (everything clockwise between lower_left and upper_left)
+        i = lower_left
+        while i != upper_left:
+            new_hull.append(left[i])
+            i = (i + 1) % len(left)  # next index clockwise
+        new_hull.append(left[i])  # add the upper_left index
+
+        if self.pause:
+            self.eraseTangent([line1])
+            self.eraseTangent([line2])
+            self.eraseHull(polygon1)
+            self.eraseHull(polygon2)
+
+        return new_hull
 
     def get_tangent_indexes(self, left, right, upper=True):
-        # Both left and right lists are sorted in clockwise order
-        left_index, right_index = self.get_inner_indexes(left, right)
+        # Both left and right lists should be sorted in clockwise order
+        left_index, right_index = get_inner_indexes(left, right)
         done = False
         while not done:
             done = True
             # Step up the left half
-            current_slope = self.get_slope(left[left_index], right[right_index])
+            current_slope = get_slope(left[left_index], right[right_index])
             next_slope = None
             while next_slope != current_slope:
-                # Next counter-clockwise index of left
+                # if getting upper tangent -> next counter-clockwise index of left
+                #            else if lower -> next clockwise index of left
                 next_left_index = ((left_index - 1) if upper else left_index + 1) % len(left)
-                next_slope = self.get_slope(left[next_left_index], right[right_index])
+                next_slope = get_slope(left[next_left_index], right[right_index])
                 if (next_slope < current_slope) if upper else (next_slope > current_slope):
-                    # We want to keep the next left index
+                    # Keep the next_left_index
                     left_index = next_left_index
                     current_slope = next_slope
                     next_slope = None
                     done = False
-                    self.blinkTangent([QLineF(left[left_index], right[right_index])], RED)
+                    if self.pause:
+                        self.blinkTangent([QLineF(left[left_index], right[right_index])], RED)
                 else:
                     next_slope = current_slope
             # Step up the right half
-            current_slope = self.get_slope(left[left_index], right[right_index])
+            current_slope = get_slope(left[left_index], right[right_index])
             next_slope = None
             while next_slope != current_slope:
-                # Next clockwise index of right
+                # if getting upper tangent -> next clockwise index of right
+                #            else if lower -> next counter-clockwise index of right
                 next_right_index = ((right_index + 1) if upper else (right_index - 1)) % len(right)
-                next_slope = self.get_slope(left[left_index], right[next_right_index])
+                next_slope = get_slope(left[left_index], right[next_right_index])
                 if (next_slope > current_slope) if upper else (next_slope < current_slope):
-                    # We want to keep the next right index
+                    # Keep the next_right_index
                     right_index = next_right_index
                     current_slope = next_slope
                     next_slope = None
                     done = False
-                    self.blinkTangent([QLineF(left[left_index], right[right_index])], RED)
+                    if self.pause:
+                        self.blinkTangent([QLineF(left[left_index], right[right_index])], RED)
                 else:
                     next_slope = current_slope
-        self.showTangent([QLineF(left[left_index], right[right_index])], GREEN)
         return left_index, right_index
-
-    def get_slope(self, point1, point2):
-        assert type(point1) == QPointF and type(point2) == QPointF
-        return (point1.y() - point2.y()) / (point1.x() - point2.x())
-
-    def get_inner_indexes(self, left, right):
-        largest_list_size = 0
-        if len(left) > len(right):
-            largest_list_size = len(left)
-        else:
-            largest_list_size = len(right)
-
-        # Get the far right point of the left list and the far left point of the right list O(n)
-        right_index = left_index = -1
-        for i in range(largest_list_size):
-            if i < len(right):
-                if right_index < 0:
-                    right_index = i
-                elif right[i].x() < right[right_index].x():
-                    right_index = i
-            if i < len(left):
-                if left_index < 0:
-                    left_index = i
-                elif left[i].x() > left[left_index].x():
-                    left_index = i
-        return left_index, right_index
-
-    # TODO delete this method. It's for testing only
-    def sort_clockwise(self, points):
-        if len(points) < 2:
-            return
-        points.sort(key=lambda item: item.y())  # Sort by y value
-        lowest = points[0]
-        highest = points[len(points) - 1]
-        middle = (lowest.y() + highest.y()) / 2
-        points.sort(key=lambda item: item.x())  # Sort by x value
-        top_half = []
-        bottom_half = []
-        for point in points:
-            if point.y() > middle:
-                top_half.append(point)
-            else:
-                bottom_half.append(point)
-        bottom_half.reverse()
-        top_half.extend(bottom_half)
-        return top_half
